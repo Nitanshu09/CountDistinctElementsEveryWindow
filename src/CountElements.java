b467df4a99564d76cea50e1d7085d60c5bedbc64
Nitanshu Sharma <nsharma1_be20@thapar.edu>
2022-10-13 07:07:31 +0530

    using HashMap

--- src/CountElements.java
+++ src/CountElements.java
@@ -4,19 +4,25 @@ public class CountElements {
 
 	public static ArrayList<Integer> countDistinct(int A[], int n, int k) {
 		ArrayList<Integer> ret = new ArrayList<Integer>();
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
 		for (int i = 0; i <= n - k; i++) {
-			int count = 0;
-			ArrayList<Integer> temp = new ArrayList<Integer>();
-			for (int j = i; j < i + k; j++) {
-				if (temp.isEmpty()) {
-					temp.add(A[j]);
-					count++;
-				} else if (!temp.contains(A[j])) {
-					temp.add(A[j]);
-					count++;
+			int start = i;
+			int end = start + k - 1;
+			if (ret.isEmpty()) {
+				while (start <= end) {
+					map.put(A[start], map.getOrDefault(A[start], 0) + 1);
+					start++;
 				}
+				ret.add(map.size());
+			} else {
+				map.put(A[end], map.getOrDefault(A[end], 0) + 1);
+				if (map.get(A[start - 1]) == 1) {
+					map.remove(A[start - 1]);
+				} else {
+					map.put(A[start - 1], map.get(A[start - 1]) - 1);
+				}
+				ret.add(map.size());
 			}
-			ret.add(count);
 		}
 		return ret;
 	}
